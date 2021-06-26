d52c4929bf84bdd9c039e66bedfba8972e7ea6ce
hatsu38 <hajiwata0308@gmail.com>
2021-06-26 20:41:36 +0900

    db:migrate

--- docker/production/entrypoint.sh
+++ docker/production/entrypoint.sh
@@ -2,6 +2,6 @@
 
 sudo service nginx start
 cd /app
-RAILS_ENV=production DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bin/rails db:schema:load
+RAILS_ENV=production bin/rails db:migrate
 RAILS_ENV=production bin/rails assets:precompile
 bundle exec pumactl start
