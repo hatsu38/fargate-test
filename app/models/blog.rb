c191da073c73cb20e042af7c1ce8e314e3513d8c
hatsu38 <hajiwata0308@gmail.com>
2021-06-26 17:44:14 +0900

    add annotate

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,2 +1,11 @@
+# == Schema Information
+#
+# Table name: blogs
+#
+#  id         :bigint           not null, primary key
+#  title      :string(255)      default(""), not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Blog < ApplicationRecord
 end
--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,2 +1,20 @@
+# == Schema Information
+#
+# Table name: employees
+#
+#  id                         :bigint           not null, primary key
+#  employee_authenticate_id   :bigint
+#  name(名前)                 :string(255)      not null
+#  created_at                 :datetime         not null
+#  updated_at                 :datetime         not null
+#
+# Indexes
+#
+#  index_employees_on_employee_authenticate_id  (employee_authenticate_id) UNIQUE
+#
+# Foreign Keys
+#
+#  fk_rails_...  (employee_authenticate_id => employee_authenticates.id)
+#
 class Employee < ApplicationRecord
 end
