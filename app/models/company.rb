ef1afb6b311acadb725cdb04fc4ac1d06cb5e3a8
hatsu38 <hajiwata0308@gmail.com>
2021-07-07 16:52:00 +0900

    Add companies

---
+++ app/models/company.rb
@@ -0,0 +1,23 @@
+# == Schema Information
+#
+# Table name: companies
+#
+#  id           :bigint           not null, primary key
+#  name(会社名) :string(255)      not null
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
+class Company < ApplicationRecord
+
+  # Relations
+
+  # Validations
+
+  # Callbacks
+
+  # Delegates
+
+  # Scopes
+
+  # Methods
+end
