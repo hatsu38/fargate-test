7d7be311859a3ea9d94f3595825464ec080c784e
hatsu38 <hajiwata0308@gmail.com>
2021-07-07 23:11:34 +0900

    Fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  resources :companies
   devise_for :employee_authenticates
   require 'sidekiq/web'
   root "blogs#index"
 
+  resources :companies
   resources :blogs
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
